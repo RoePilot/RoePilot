8008e2855acd80fd68b9e29c4b7e36ac8d3fa916
Alvi Hossain <alvi997@outlook.com>
2025-03-11 13:28:21 +0000

    database

--- pg-sd2/app/services/db.js
+++ pg-sd2/app/services/db.js
@@ -3,25 +3,29 @@ require("dotenv").config();
 const mysql = require('mysql2/promise');
 
 const config = {
-  db: { /* do not put password or any sensitive info here, done only for demo */
-    host: process.env.DB_CONTAINER,
-    port: process.env.DB_PORT,
-    user: process.env.MYSQL_ROOT_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+  db: {
+    host: process.env.DB_CONTAINER || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.MYSQL_ROOT_USER || 'root',
+    password: process.env.MYSQL_ROOT_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || 'Roepilot', // Defaults to "Roepilot"
     waitForConnections: true,
     connectionLimit: 2,
     queueLimit: 0,
   },
 };
-  
+
 const pool = mysql.createPool(config.db);
 
 // Utility function to query the database
 async function query(sql, params) {
-  const [rows, fields] = await pool.execute(sql, params);
-
-  return rows;
+  try {
+    const [rows, fields] = await pool.execute(sql, params);
+    return rows;
+  } catch (error) {
+    console.error("Database query error:", error);
+    throw error;
+  }
 }
 
 module.exports = {
