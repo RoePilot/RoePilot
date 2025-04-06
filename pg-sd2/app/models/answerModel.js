961cb1957871fe77abd7c7ca5eeea700e2ffa666
sukhjeet singh sekhons <sukhjeetsekhon007@gmail.com>
2025-04-06 22:10:37 +0100

    ans js

--- pg-sd2/app/models/answerModel.js
+++ pg-sd2/app/models/answerModel.js
@@ -1,9 +1,19 @@
-function downvoteAnswer(answerId) {
-  const sql = "UPDATE answers SET NumOfUpvote = NumOfUpvote - 1 WHERE AnswerID = ? AND NumOfUpvote > 0";
+const db = require('../services/db');
+
+// Define the upvote function
+function upvoteAnswer(answerId) {
+  const sql = "UPDATE answers SET NumOfUpvote = NumOfUpvote + 1 WHERE AnswerID = ?";
   return db.query(sql, [answerId]);
 }
 
+// Define the downvote function
+function downvoteAnswer(answerId) {
+  const sql = "UPDATE answers SET NumOfUpvote = CASE WHEN NumOfUpvote > 0 THEN NumOfUpvote - 1 ELSE 0 END WHERE AnswerID = ?";
+  return db.query(sql, [answerId]);
+}
+
+// Export both
 module.exports = {
   upvoteAnswer,
-  downvoteAnswer // 
+  downvoteAnswer
 };
